01b0349cc1f26b6cb7ea3d9aa88fb7779bcc5e51
Atibhi Agrawal <atibhi.a@gmail.com>
2022-03-02 20:32:45 +0530

    testing

--- src/stirling/source_connectors/socket_tracer/protocols/thrift/parse.cc
+++ src/stirling/source_connectors/socket_tracer/protocols/thrift/parse.cc
@@ -64,16 +64,115 @@ namespace thrift {
         // std::cout<<buf->size()<<" buf size:\n";
         
         BinaryDecoder decoder(*buf);
-
-        tm.version = decoder.ExtractInt<uint16_t>().ValueOr(0xffff);
-
-        // std::cout<<tm.version <<"tm.version \n\n";
+    
+        PL_ASSIGN_OR(tm.version, decoder.ExtractInt<uint16_t>(),
+                 return ParseState::kInvalid);
        
         if(tm.version != 0x8001) {
             std::cout<<"Payload does not conform to thrift standard:"<< tm.version<<"\n";
         }
 
-       
+
+        PL_ASSIGN_OR(tm.unused, decoder.ExtractInt<uint8_t>(),
+                 return ParseState::kInvalid);
+        // std::cout<<tm.unused<<" :unused\n";
+
+         PL_ASSIGN_OR(tm.message_type, decoder.ExtractInt<uint8_t>(),
+                 return ParseState::kInvalid);        
+        // std::cout<<tm.message_type<<" :message_type\n";
+        if( tm.message_type <1 || tm.message_type>4) {
+            std::cout<<"Payload does not conform to thrift standard:"<< tm.message_type<<" leading bits in message type is less than 5\n";
+        }
+
+        PL_ASSIGN_OR(tm.name_length, decoder.ExtractInt<int32_t>(),
+                 return ParseState::kInvalid);
+        //  std::cout<<"name len: "<<tm.name_length<<"\n"; 
+         if( tm.name_length<0) {
+            std::cout<<"Payload does not conform to thrift standard:"<< tm.name_length<<"\n";
+        }
+
+        PL_ASSIGN_OR(tm.name, decoder.ExtractInt<int64_t>(),
+                 return ParseState::kInvalid);
+        // std::cout<<"name: "<<tm.name<<"\n"; 
+
+
+        PL_ASSIGN_OR(tm.sequence_id, decoder.ExtractInt<int32_t>(),
+                 return ParseState::kInvalid);
+        // std::cout<<"seq_id: "<<tm.sequence_id<<"\n";    
+
+        while(1) {
+            int8_t  fieldType = 0;
+            PL_ASSIGN_OR(fieldType, decoder.ExtractInt<int8_t>(),
+                 return ParseState::kInvalid);
+            std::cout<<fieldType<<"fieldType\n";
+            
+            if(fieldType==0) {
+                std::cout<<"Break\n";
+                 break;
+            }
+
+            int16_t fieldID = 0;
+             PL_ASSIGN_OR(fieldID, decoder.ExtractInt<int16_t>(),
+                 return ParseState::kInvalid);
+            std::cout<<fieldID<<"fieldID\n";
+
+            switch(fieldType) {
+                        case ThirftBool: {
+                //             bool booleanValue = false;
+                //              PL_ASSIGN_OR(booleanValue, decoder.ExtractInt<bool>(),
+                //  return ParseState::kInvalid);
+                // hi
+                            continue;
+                        }
+                        case ThriftInt8: {
+                            continue;
+
+                        }
+                        case ThriftDouble: {
+                            continue;
+
+                        }
+                        // case ThriftInt16: {
+                        //     continue;
+
+                        // }
+                        case ThriftInt32: {
+                            continue;
+
+                        }
+                        // case ThriftInt64: {
+                        //     continue;
+
+                        // }
+                        case ThriftStringAndBinary: {
+                            continue;
+
+                        }
+                        case ThriftStruct: {
+                            continue;
+
+                        }
+                        case ThriftMap: {
+                            continue;
+
+                        }
+                        case ThriftSet: {
+                            continue;
+
+                        } 
+                        case ThriftList: {
+                            continue;
+
+                        }
+                        default: {
+                            std::cout<<"None of the types match\n\n";
+                        }
+            }
+            
+        }
+
+
+
         return ParseState::kSuccess;
     }
 
